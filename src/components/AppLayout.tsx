167ab585f633ff0421309be99688660973c9e185
Moiz Mirza <moizmirza212@gmail.com>
2025-08-06 18:48:04 +0500

    Fix

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -2,7 +2,7 @@
 "use client";
 import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
-import MainLayout from "./MainLayout";
+import MainLayout from "./MainLayout.tsx";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   const { data: session, status } = useSession();
