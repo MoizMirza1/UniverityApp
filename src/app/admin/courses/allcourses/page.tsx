0820476333761ab8352ece6b9d08c60e5021988b
Moiz Mirza <moizmirza212@gmail.com>
2025-08-12 01:46:49 +0500

    Update the yml

--- src/app/admin/courses/allcourses/page.tsx
+++ src/app/admin/courses/allcourses/page.tsx
@@ -9,8 +9,8 @@ const page = () => {
         title="All Course"
         breadcrumbs={["Courses", "All Courses"]}
       />
-      <AllCourses />  
-      
+      <AllCourses />
+
     </div>
   )
 }
